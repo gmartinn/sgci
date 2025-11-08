b0ab15e94e9434c0e2cb4a3af51daf91b850b200
Guilherme Martin <72753826+gmartinn@users.noreply.github.com>
2025-11-07 22:19:20 -0300

    Finalizada primeira parte

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,21 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl underline">Home</h1>
-      <Button>Click Me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image 
+          src="/assets/icons/logo-full.svg"
+          height={1000}
+          width={1000}
+          alt="patient"
+          className="mb-12 h-10 w-fit"
+          />
+
+          
+        </div>
+
+      </section>
     </div>
   );
 }
